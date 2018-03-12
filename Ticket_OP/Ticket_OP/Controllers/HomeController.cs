b15b6b1de25ebfea00687608d81b43dad0324a3f
สุปรีชา นิรมลพานิช <film_ten@hotmail.com>
2018-03-12 11:18:04 +0700

    COmmit 12/03/61 , 11:17

--- Ticket_OP/Ticket_OP/Controllers/HomeController.cs
+++ Ticket_OP/Ticket_OP/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using Ticket_OP.Models;
 
 namespace Ticket_OP.Controllers
 {
@@ -25,6 +28,86 @@ namespace Ticket_OP.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult SandEmail()
+        {
+            
+
+            return View();
+        }
+
+        public ActionResult success()
+        {
+
+
+            return View();
+        }
+
+        public ActionResult SandEmail_S(SandEmail mail)
+        {
+            GetEmail Email = new GetEmail();
+
+            Email.FULLNAME = mail.FULLNAME;
+            Email.EMAIL = mail.EMAIL;
+            Email.PHONE = mail.PHONE;
+            Email.MESSAGE = mail.MESSAGE;
+
+            SendMail(Email);
+
+            return RedirectToAction("success", "Home");
+        }
+        //SandEmail ticket, int mode = 0
+        protected void SendMail(GetEmail Email)
+        {
+            string fnt1 = "<font size='5' face='Angsana New'>";
+            string fnt2 = "</font>";
+            //string url = global::System.Configuration.ConfigurationManager.AppSettings["Beauty.Host"].ToString();
+
+            try
+            {
+                MailMessage mail = new MailMessage();
+
+                mail.From = new MailAddress("itticket@beautycommunity.co.th", "International Business");
+
+                mail.To.Add("supreecha.nir@beautycommunity.co.th");
+                mail.Subject = "Inquiry BeautyCommunity";
+
+                //if (devdetail.EMAIL != null && devdetail.EMAIL.Length > 10)
+                //{
+                //    mail.To.Add(devdetail.EMAIL_CRE);
+                //}
+                //else{
+                //    mail.Subject = "ปิด Ticket ! " + devdetail.TOPIC;
+                //}
+
+                mail.IsBodyHtml = true;
+                StringBuilder sb = new StringBuilder();
+
+
+                //url = url + "/Dev/DevDetail?DevId=" + devdetail.DV_ID.ToString();
+
+
+                    sb.Append($"<br><b>{fnt1} มีผู้สมัครงานใหม่ !!{fnt2}</b><hr>");
+
+                sb.Append($"<br><strong>{fnt1}ชื่อ-นามสกุล</strong> : {Email.FULLNAME}{fnt2}");//ชื่อ-นามสกุล
+                sb.Append($"<br><strong>{fnt1}อีเมลติดต่อ</strong> : {Email.EMAIL}{fnt2}");//อีเมลติดต่อกลับ
+                sb.Append($"<br><strong>{fnt1}เบอร์ต่อต่อ</strong> : {Email.PHONE}{fnt2}");//เบอร์ติดต่อ
+                sb.Append($"<br><strong>{fnt1}รายละเอียด</strong> : {Email.MESSAGE}{fnt2}");//รายละเอียด
+
+
+                mail.Body = sb.ToString();
+
+
+                SmtpClient stmp = new SmtpClient();
+                stmp.Send(mail);
+
+                ViewBag.ErrorMessage = "Success";
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ErrorMessage = ex.Message;
+            }
         }
     }
 }
---
+++ Ticket_OP/Ticket_OP/Models/SandEmail.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Ticket_OP.Models
+{
+    public class SandEmail
+    {
+        //public SelectList JT_LIST { get; set; }
+
+        [Required(ErrorMessage = "กรุณาใส่ชื่อนามสกุล")]
+        [Display(Name = "ชื่อ-นามสกุล")]
+        public string FULLNAME { get; set; }
+
+        [Required(ErrorMessage = "กรุณาใส่อีเมล")]
+        [Display(Name = "อีเมล")]
+        public string EMAIL { get; set; }
+
+        [Required(ErrorMessage = "กรุณาใส่เบอร์ติดต่อ")]
+        [Display(Name = "เบอร์ติดต่อ")]
+        public string PHONE { get; set; }
+
+        [Required(ErrorMessage = "กรุณาใส่รายละเอียด")]
+        [Display(Name = "รายละเอียด")]
+        public string MESSAGE { get; set; }
+
+    }
+
+    public class GetEmail
+    {
+        //public SelectList JT_LIST { get; set; }
+
+        [Required(ErrorMessage = "กรุณาใส่ชื่อนามสกุล")]
+        [Display(Name = "ชื่อ-นามสกุล")]
+        public string FULLNAME { get; set; }
+
+        [Required(ErrorMessage = "กรุณาใส่อีเมล")]
+        [Display(Name = "อีเมล")]
+        public string EMAIL { get; set; }
+
+        [Required(ErrorMessage = "กรุณาใส่เบอร์ติดต่อ")]
+        [Display(Name = "เบอร์ติดต่อ")]
+        public string PHONE { get; set; }
+
+        [Required(ErrorMessage = "กรุณาใส่รายละเอียด")]
+        [Display(Name = "รายละเอียด")]
+        public string MESSAGE { get; set; }
+
+    }
+}
